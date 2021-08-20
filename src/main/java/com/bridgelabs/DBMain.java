5d4c1568cf65c2c221625e88ccfce26984e1132a
yashpurwar1 <yashpurwar.yashpurwar.com>
2021-08-20 23:49:47 +0530

    Added: Ability to create connection with the database

--- src/main/java/com/bridgelabs/DBMain.java
+++ src/main/java/com/bridgelabs/DBMain.java
@@ -1,7 +1,45 @@
 package com.bridgelabs;
 
+import java.sql.Connection;
+import java.sql.Driver;
+import java.sql.DriverManager;
+import java.util.Enumeration;
+
+
 public class DBMain {
     public static void main(String[] args) {
-        System.out.println("Welcome to employee payroll db");
+        String jdbcURL = "jdbc:mysql://localhost:3306/payroll_service?useSSL=false";
+        String userName = "root";
+        String password = "yash76652";
+        Connection connection;
+
+        try {
+            Class.forName("com.mysql.cj.jdbc.Driver");
+            System.out.println("Driver Loaded.");
+        } catch (ClassNotFoundException e) {
+            throw new IllegalStateException("Cannot find the driver in the classpath!", e);
+        }
+
+        listDrivers();
+
+        // Establishing connection.
+        try {
+            System.out.println("Connecting to database: " + jdbcURL);
+            connection = DriverManager.getConnection(jdbcURL, userName, password);
+            System.out.println("Connection is successful! " + connection);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+    /**
+     * Purpose : Check all the drivers registered with the jdbc driver
+     */
+    private static void listDrivers() {
+        Enumeration<Driver> driverList = DriverManager.getDrivers();
+        while (driverList.hasMoreElements()) {
+            Driver driverClass = driverList.nextElement();
+            System.out.println(driverClass.getClass().getName());
+        }
     }
 }
